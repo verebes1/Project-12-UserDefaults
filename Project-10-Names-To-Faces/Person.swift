ff77341de3f43cb4cc5d7b6e7311f5252202f301
David Verbenyi <dverbenyi@gmail.com>
2018-06-28 14:05:38 +0100

    Initial Commit

---
+++ Project-10-Names-To-Faces/Person.swift
@@ -0,0 +1,31 @@
+//
+//  Person.swift
+//  Project-10-Names-To-Faces
+//
+//  Created by verebes on 26/06/2018.
+//  Copyright © 2018 A&D Progress. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject, NSCoding {
+    
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        name = aDecoder.decodeObject(forKey: "name") as! String
+        image = aDecoder.decodeObject(forKey: "image") as! String
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: "name")
+        aCoder.encode(image, forKey: "image")
+    }
+
+}
---
+++ Project-10-Names-To-Faces/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project-10-Names-To-Faces
+//
+//  Created by verebes on 21/06/2018.
+//  Copyright © 2018 A&D Progress. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+}
